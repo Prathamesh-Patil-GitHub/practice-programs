86b56461ed818e3df68671cf76032ff95899e951
Prathamesh Patil <89769306+Prathamesh-Patil-GitHub@users.noreply.github.com>
2023-04-29 01:09:24 +0530

    Delete CH- 1 AWT Container and Event Handling directory

--- CH- 1 AWT Container and Event Handling/FirstGUIApp.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Frame;
-class FirstGUIApp{
-    public static void main(String[] args){
-        Frame f=new Frame();
-        f.setSize(600,600);
-        f.setTitle("First GUI Application");
-        f.setVisible(true);
-    }
-}
