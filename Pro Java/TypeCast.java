4a9ee63edd75873785b32fb6677f92a0094952e7
Prathamesh Patil <89769306+Prathamesh-Patil-GitHub@users.noreply.github.com>
2023-04-28 12:31:51 -0700

    Added Java Programs

---
+++ Pro Java/TypeCast.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class TypeCast
+{
+	public static void main(String[] args)
+	{
+		short x;
+		char ch='A';
+		x=(short)ch;
+		System.out.println("Ascii code of A:"+x);
+	}
+
+}
