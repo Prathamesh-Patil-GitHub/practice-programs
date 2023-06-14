fa3ba88129aa5f863cc56d57defc05a22f421fb0
Prathamesh Patil <89769306+Prathamesh-Patil-GitHub@users.noreply.github.com>
2023-06-14 18:55:31 +0530

    Updated DSA Engineering Lab Programs

--- PRO Data Structures/DSA in C++ Engineering LAB/DSA Sem 4/8 Bellman Ford Algorithm/bellman-ford.cpp
+++ PRO Data Structures/DSA in C++ Engineering LAB/DSA Sem 4/8 Bellman Ford Algorithm/bellman-ford.cpp
@@ -26,9 +26,9 @@ int main() {
     };
     vector<int> distance(N, INF);
     distance[0] = 0;
-    
+
     cout<<"\n'          - BELLMAN FORD ALGORITHM -        '\n\n";
-    
+
     cout<<"Before applying algorithm distance vector is: "<<endl;
     for(int num: distance){
         if(num == INF)
@@ -36,36 +36,43 @@ int main() {
         else
             cout<<num<<" ";
     }
-    
+
     for(int i=0;i<N-1;i++){
         relax(i, graph, distance);
     }
-  
-    vector<int> temp {distance};
-    
-    for(int i=0;i<N-1;i++){
-        relax(i, graph, distance);
-    }
-    
-    cout<<"\n\nAfter applying algorithm distance vector is: "<<endl;
+
+    cout<<"\n\nAfter applying algorithm once distance vector is: "<<endl;
     for(int num: distance){
         if(num == INF)
             cout<<"INF ";
         else
             cout<<num<<" ";
     }
-    
+    vector<int> temp {distance};
+
+    for(int i=0;i<N-1;i++){
+        relax(i, graph, distance);
+    }
+
+    cout<<"\n\nAfter applying algorithm twice distance vector is: "<<endl;
+    for(int num: distance){
+        if(num == INF)
+            cout<<"INF ";
+        else
+            cout<<num<<" ";
+    }
+
     // Checking for negative cycle
     bool isPresent = false;
     for(int i=0; i<N; i++){
         if(distance[i] != temp[i]){
-            cout<<"\nNegative Cycle Exists !"<<endl;
+            cout<<"\n\nNegative Cycle Exists !"<<endl;
             isPresent = true;
             break;
         }
     }
     if(!isPresent){
-        cout<<"\nNegative Cycle Doesn't Exists !"<<endl;
+        cout<<"\n\nNegative Cycle Doesn't Exists !"<<endl;
     }
     return 0;
 }
@@ -82,6 +89,22 @@ vector<vector<int>> graph =  {
         {INF, INF, INF, INF, INF, 3},
         {INF, INF, INF, INF, INF, INF}
     };
+
+OUTPUT:
+
+'          - BELLMAN FORD ALGORITHM -        '
+
+Before applying algorithm distance vector is:
+0 INF INF INF INF INF
+
+After applying algorithm once distance vector is:
+0 2 3 5 5 4
+
+After applying algorithm twice distance vector is:
+0 1 3 5 1 4
+
+Negative Cycle Exists !
+
 */
 /* TEST CASE 1:
 
@@ -93,9 +116,20 @@ N = 5
         {INF, INF, -2, INF, INF},
         {INF, INF, INF, 6, INF},
     };
+
+OUTPUT:
+
+'          - BELLMAN FORD ALGORITHM -        '
+
+Before applying algorithm distance vector is:
+0 INF INF INF INF
+
+After applying algorithm once distance vector is:
+0 3 1 3 1
+
+After applying algorithm twice distance vector is:
+0 -1 -3 -1 -2
+
+Negative Cycle Exists !
+
 */
-
-
-
-
-
